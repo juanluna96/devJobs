a714354dd978e247a9b24ad2ded3e3e4d696fcf4
juanluna96 <juandluna17@gmail.com>
2021-01-09 15:54:32 -0500

    Creando un Select para las categorías

--- resources/views/vacantes/create.blade.php
+++ resources/views/vacantes/create.blade.php
@@ -18,6 +18,22 @@
                 </p>
             @enderror
         </div>
+        <div class="my-3">
+            <label for="categoria" class="block text-gray-700 text-sm mb-2"> Categoria: </label>
+            <select name="categoria" id="categoria" class="block appearance-none w-full border border-gray-200 text-gray-700 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500 p-3 bg-white">
+                <option disabled selected>-- Selecciona --</option>
+
+                @foreach ($categorias as $categoria)
+                    <option value="{{$categoria->id}}">{{ $categoria->nombre }}</option>
+                @endforeach
+            </select>
+
+            @error('titulo')
+                <p class="bg-red-100 border-l-4 border-red-500 p-4 w-full text-red-500 text-sm italic mt-4" role="alert">
+                    {{ $message }}
+                </p>
+            @enderror
+        </div>
 
         <button type="submit" class="bg-teal-500 w-full hover:bg-teal-600 text-gray-100 font-bold p-3 focus:outline focus:shadow-outline uppercase">Publicar vacante</button>
     </form>
