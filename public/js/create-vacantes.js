c979eaaa0c2c1ac8e1daadccbb529d7f51e019b6
juanluna96 <juandluna17@gmail.com>
2021-01-17 07:51:44 -0500

    Trabajando con los métodos de Dropzone

---
+++ public/js/create-vacantes.js
@@ -0,0 +1,51 @@
+Dropzone.autoDiscover = false;
+document.addEventListener('DOMContentLoaded', () => {
+
+    /* ------------------------------ Medium editor ----------------------------- */
+
+    const editor = new MediumEditor('.editable', {
+        toolbar: {
+            buttons: ['bold', 'italic', 'underline', 'quote', 'anchor', 'justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull', 'orderedList', 'unorderedList', 'h2', 'h3'],
+            static: true,
+            sticky: true,
+        },
+        placeholder: {
+            text: 'Informacion de la vacante'
+        }
+    });
+    editor.subscribe('editableInput', function(eventObj, editable) {
+        const contenido = editor.getContent();
+        document.querySelector('#descripcion').value = contenido;
+    });
+
+    /* -------------------------------- Dropzone -------------------------------- */
+
+
+    const dropzoneDevJobs = new Dropzone('#dropzoneDevJobs', {
+        url: '/vacantes/imagen',
+        dictDefaultMessage: 'Sube aquí tu archivo',
+        acceptedFiles: '.png,.jpg,.jpeg,.gif,.bmp',
+        addRemoveLinks: true,
+        maxFiles: 1,
+        dictRemoveFile: 'Borrar archivo',
+        headers: {
+            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content
+        },
+        success: function(file, response) {
+            console.log(response)
+        },
+        error: function(file, response) {
+            console.log(response);
+            document.querySelector('#error').textContent = 'Formato no valido';
+        },
+        maxfilesexceeded: function(file) {
+            if (this.files[1] != null) {
+                this.removeFile(this.files[0]); //Elimina el archivo anterior
+                this.addFile(file); //Agregar el nuevo archivo
+            }
+        },
+        removedfile: function(file, response) {
+            console.log(file);
+        }
+    })
+})
---
+++ public/js/dist/create-vacantes.dev.js
@@ -0,0 +1,50 @@
+"use strict";
+
+Dropzone.autoDiscover = false;
+document.addEventListener('DOMContentLoaded', function () {
+  /* ------------------------------ Medium editor ----------------------------- */
+  var editor = new MediumEditor('.editable', {
+    toolbar: {
+      buttons: ['bold', 'italic', 'underline', 'quote', 'anchor', 'justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull', 'orderedList', 'unorderedList', 'h2', 'h3'],
+      "static": true,
+      sticky: true
+    },
+    placeholder: {
+      text: 'Informacion de la vacante'
+    }
+  });
+  editor.subscribe('editableInput', function (eventObj, editable) {
+    var contenido = editor.getContent();
+    document.querySelector('#descripcion').value = contenido;
+  });
+  /* -------------------------------- Dropzone -------------------------------- */
+
+  var dropzoneDevJobs = new Dropzone('#dropzoneDevJobs', {
+    url: '/vacantes/imagen',
+    dictDefaultMessage: 'Sube aquí tu archivo',
+    acceptedFiles: '.png,.jpg,.jpeg,.gif,.bmp',
+    addRemoveLinks: true,
+    maxFiles: 1,
+    dictRemoveFile: 'Borrar archivo',
+    headers: {
+      'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content
+    },
+    success: function success(file, response) {
+      console.log(response);
+    },
+    error: function error(file, response) {
+      console.log(response);
+      document.querySelector('#error').textContent = 'Formato no valido';
+    },
+    maxfilesexceeded: function maxfilesexceeded(file) {
+      if (this.files[1] != null) {
+        this.removeFile(this.files[0]); //Elimina el archivo anterior
+
+        this.addFile(file); //Agregar el nuevo archivo
+      }
+    },
+    removedfile: function removedfile(file, response) {
+      console.log(file);
+    }
+  });
+});
