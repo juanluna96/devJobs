86fab8f7fb7ca064f22ce3f6273e3c5429fbfccb
juanluna96 <juandluna17@gmail.com>
2021-01-17 21:00:26 -0500

    Validando el Titulo

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 // Rutas de vacantes
 Route::get('/vacantes', 'VacanteController@index')->name('vacantes.index');
 Route::get('/vacantes/create', 'VacanteController@create')->name('vacantes.create');
+Route::post('/vacantes', 'VacanteController@store')->name('vacantes.store');
 
 // Subir imagenes
 Route::post('/vacantes/imagen', 'VacanteController@imagen')->name('vacantes.imagen');
