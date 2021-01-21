f6a8f47fa6a7c904732efceed22737080c3d075d
juanluna96 <juandluna17@gmail.com>
2021-01-20 21:03:08 -0500

    Creando la ruta para mostrar una vacante

--- app/Http/Controllers/VacanteController.php
+++ app/Http/Controllers/VacanteController.php
@@ -93,7 +93,7 @@ class VacanteController extends Controller
      */
     public function show(Vacante $vacante)
     {
-        //
+        return view('vacantes.show')->with('vacante', $vacante);
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,17 @@ Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-// Rutas de vacantes
-Route::get('/vacantes', 'VacanteController@index')->name('vacantes.index');
-Route::get('/vacantes/create', 'VacanteController@create')->name('vacantes.create');
-Route::post('/vacantes', 'VacanteController@store')->name('vacantes.store');
+// Rutas protegidas
+Route::group(['middleware' => ['auth', 'verify']], function () {
+    // Rutas de vacantes
+    Route::get('/vacantes', 'VacanteController@index')->name('vacantes.index');
+    Route::get('/vacantes/create', 'VacanteController@create')->name('vacantes.create');
+    Route::post('/vacantes', 'VacanteController@store')->name('vacantes.store');
 
-// Subir imagenes
-Route::post('/vacantes/imagen', 'VacanteController@imagen')->name('vacantes.imagen');
-Route::post('/vacantes/borrarimagen', 'VacanteController@borrarimagen')->name('vacantes.borrar');
+    // Subir imagenes
+    Route::post('/vacantes/imagen', 'VacanteController@imagen')->name('vacantes.imagen');
+    Route::post('/vacantes/borrarimagen', 'VacanteController@borrarimagen')->name('vacantes.borrar');
+});
+
+// Muestra los trabajos en el front end sin verification
+Route::get('/vacantes/{vacante}', 'VacanteController@show')->name('vacantes.show');
