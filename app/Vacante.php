2e9c499789a990fecf54480305f5b1ef62570b3d
juanluna96 <juandluna17@gmail.com>
2021-01-21 13:25:04 -0500

    Mostrando el Nombre del Reclutador

--- app/Vacante.php
+++ app/Vacante.php
@@ -30,4 +30,10 @@ class Vacante extends Model
     {
         return $this->belongsTo(Experiencia::class);
     }
+
+    // Relacion 1:1 reclutador y vacante
+    public function reclutador()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
