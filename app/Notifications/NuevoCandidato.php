abbdb472ec522b7c415fbe8fd9ba9c1b47c6a837
juanluna96 <juandluna17@gmail.com>
2021-01-23 22:31:22 -0500

    Almacenar las Notificaciones

--- app/Notifications/NuevoCandidato.php
+++ app/Notifications/NuevoCandidato.php
@@ -29,7 +29,7 @@ class NuevoCandidato extends Notification
      */
     public function via($notifiable)
     {
-        return ['mail'];
+        return ['mail', 'database'];
     }
 
     /**
@@ -45,6 +45,15 @@ class NuevoCandidato extends Notification
             ->line('La vacante es: ' . $this->vacante)
             ->action('Visita DevJobs', url('/'))
             ->line('Gracias por usar DevJobs!');
+    }
+
+    // Notificaciones en la BD
+
+    public function toDatabase($notifiable)
+    {
+        return [
+            'vacante' => $this->vacante
+        ];
     }
 
     /**
