fcde3879f8800d054afd84a2966781c169873f9c
juanluna96 <juandluna17@gmail.com>
2021-01-27 12:59:37 -0500

    Creando el Banner Principal

--- resources/views/inicio/index.blade.php
+++ resources/views/inicio/index.blade.php
@@ -5,5 +5,19 @@
 @endsection
 
 @section('content')
-    <h1 class="text-2xl text-center mt-10">Administrar Vacantes</h1>
+    <div class="flex flex-col lg:flex-row shadow bg-white">
+        <div class="lg:w-1/2 px-8 lg:px-12 py-12 lg:py-24">
+            <p class="text-2xl text-gray-700">
+                Dev<span class="font-bold">Jobs</span>
+            </p>
+            <h1 class="mt-2 sm:mt-4 text-3xl text-gray-700 leading-tight">
+                Encuentra un trabajo remoto o en tu pais<span class="text-teal-500 font-bold block">Para
+                    Desarrolladores/Diseñadores
+                    web</span>
+            </h1>
+        </div>
+        <div class="block lg:w-1/2">
+            <img class="inset-0 h-full w-full object-cover object-right" src="/img/4321.jpg" alt="DevJobs" srcset="">
+        </div>
+    </div>
 @endsection
